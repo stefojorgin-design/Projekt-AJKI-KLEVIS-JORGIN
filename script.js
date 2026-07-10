456e6ac3721ace0582404d0d06199903f27d33c2
stefojorgin-design <stefojorgin@gmail.com>
2026-07-10 10:51:09 +0200

    Bere gave te mos punosh i ziu ti

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const games = [
   },
   {
     title: "2D war game",
-    url: "tank%20battle/index.html;",
+    url: "file:///Users/student/tank%20battle/index.html;",
     description: "A 2D war game where you may fight for your country."
   },
   {
