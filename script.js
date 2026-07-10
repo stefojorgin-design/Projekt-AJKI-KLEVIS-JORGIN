4f442fd30931dd9ede5dd625ab6148a5a0602c66
stefojorgin-design <stefojorgin@gmail.com>
2026-07-10 10:49:46 +0200

    Refactor game list and rendering logic

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const games = [
   },
   {
     title: "2D war game",
-    url: "/Users/student/tank%20battle/index.html;",
+    url: "tank%20battle/index.html;",
     description: "A 2D war game where you may fight for your country."
   },
   {
